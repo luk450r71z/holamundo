bfd42bb5849a51e329147de5703c6eea5803e7ad
luk450r71z <lucasdjbg@gmail.com>
2021-09-22 11:20:51 -0300

    sdfs
    sdfsdf

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 echo "hola mundo<br>";
 echo $_SERVER['SERVER_NAME']."<br>";
 echo $_SERVER['HTTP_HOST']."<br>";
+echo $_SERVER['REMOTE_ADDR']."<br>";echo $_SERVER['SERVER_ADDR']."<br>";
+
 echo "re-build";
 
 ?>
